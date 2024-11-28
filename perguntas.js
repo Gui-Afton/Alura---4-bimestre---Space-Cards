53b6dd4d2894902071cc9a01b57514479422ea46
Guilherme Righi <0000109460060xsp@al.educacao.sp.gov.br>
2024-11-28 20:17:30 -0300

    Adição do projeto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    "Fun Fact",
+    "Curiosidade",
+    "Sabia que, é possível alinhar todos os planetas do sistema solar entre a lua e a Terra?"
+)
+
+criaCartao(
+    "Noção Espacial",
+    "Qual a distância da Terra para a Lua?",
+    "sua distância em relação ao nosso planeta varia, mas em média, a distância da Terra à Lua é de aproximadamente 384.400 km."
+)
+
+criaCartao(
+    "Equipamentos",
+    "Quais são os equipamentos mais famosos?",
+    "É o telescópio, ele permite a observação de objetos celestes distantes, como estrelas, planetas e galáxias, ampliando a luz que chega até nós"
+)
+
+criaCartao(
+    "Sondas",
+    "O que são Voyager 1 e Voyager 2",
+    "Voyager 1 e 2 são sondas espaciais lançadas pela NASA em 1977 com a missão de explorar o nosso sistema solar e além dele. Ambas as sondas carregam o Disco de Ouro, que contém sons e imagens selecionados para retratar a diversidade da vida e da cultura na Terra, destinado a qualquer forma de vida extraterrestre que possa encontrá-las no futuro. A Voyager 1 se tornou a primeira sonda a entrar no espaço interestelar, ou seja, está totalmente fora do nosso sistema solar, o que aconteceu em 2012, enquanto Voyager 2, em 2018."
+)
